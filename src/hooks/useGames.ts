55f3205e4cdc27ea8bbda31ce98cbc44c6d07445
g.musciagli <gionmuscia@gmail.com>
2024-03-16 11:06:14 +0100

    Sort the Games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,7 @@ const useGames = (gameQuery: GameQuery) => useData<Game>
     '/games',
     {
       params:
-          {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id }},
+          {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder }},
     [gameQuery]
 )
 
